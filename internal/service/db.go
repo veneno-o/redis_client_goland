d1540788af38c8a3f72e4baa022c97f60bfee1c3
veneno <1929750203@qq.com>
2023-11-18 08:06:56 +0800

    feat: get dbList func

--- internal/service/db.go
+++ internal/service/db.go
@@ -6,28 +6,61 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 var ctx context.Context
 
 func DbList(identity string) ([]*define.DbItem, error) {
+	var nums int
+	res := map[string]int{}
 	if identity == "" {
 		return nil, errors.New("唯一标识不能为空")
 	}
-	conn, err := helper.GetConnection(identity)
-	if err != nil {
-		return nil, err
-	}
-	rdb, err := helper.GetRedisClient(conn, 0)
-	if err != nil {
-		return nil, err
-	}
-	// info 获取不同建个数
+	connection, _ := helper.GetConnection(identity)
+	rdb, _ := helper.GetRedisClient(connection, 0)
+
 	keySpace, err := rdb.Info(context.Background(), "keyspace").Result()
-	// config get 获取数据库个数
-	fmt.Printf("keySpace:%#v", keySpace)
 	if err != nil {
-		return nil, nil
+		return nil, errors.New("数据执行有误" + err.Error())
 	}
-	return nil, nil
+	dbList := strings.Split(keySpace, "\r\n")
+	for i, v := range dbList {
+		if i == 0 || v == "" {
+			continue
+		}
+		key := strings.Split(v, ":")
+		if len(key) < 2 {
+			continue
+		}
+		keyValue := strings.Split(key[1], ",")
+		for _, vv := range keyValue {
+			arr := strings.Split(vv, "=")
+			if len(arr) < 2 {
+				continue
+			}
+			nums, _ = strconv.Atoi(arr[1])
+			if arr[0] == "keys" {
+				res[key[0]] = nums
+			}
+		}
+	}
+	// config get 获取数据库个数
+	result, _ := rdb.ConfigGet(context.Background(), "databases").Result()
+
+	n, _ := strconv.Atoi(result[1].(string))
+	dbs := make([]*define.DbItem, n)
+	for i, _ := range dbs {
+		item := &define.DbItem{
+			Key:    fmt.Sprintf("key%d", i),
+			Number: 0,
+		}
+
+		if n, ok := res[fmt.Sprintf("key%d", i)]; ok {
+			item.Number = n
+		}
+		dbs[i] = item
+	}
+	return dbs, nil
 }
--- test/db_test.go
+++ test/db_test.go
@@ -1,9 +1,12 @@
 package test
 
 import (
+	"changeme/internal/define"
 	"context"
 	"fmt"
 	"github.com/go-redis/redis/v8"
+	"strconv"
+	"strings"
 	"testing"
 )
 
@@ -13,8 +16,8 @@ var rdb = redis.NewClient(&redis.Options{
 var ctx = context.Background()
 
 func TestInfo(t *testing.T) {
-	//var nums int
-	//res := map[string]int{}
+	var nums int
+	res := map[string]int{}
 	options := redis.Options{
 		Addr:         "localhost:6379",
 		Username:     "",
@@ -24,31 +27,46 @@ func TestInfo(t *testing.T) {
 		WriteTimeout: -1,
 	}
 	rdb := redis.NewClient(&options)
-	//keySpace, err := rdb.Info(context.Background(), "keyspace").Result()
-	//if err != nil {
-	//	fmt.Printf("数据执行有误" + err.Error())
-	//}
-	//dbList := strings.Split(keySpace, "\r\n")
-	//for i, v := range dbList {
-	//	if i == 0 || v == "" {
-	//		continue
-	//	}
-	//	key := strings.Split(v, ":")
-	//	if len(key) < 2 {
-	//		continue
-	//	}
-	//	keyValue := strings.Split(key[1], ",")
-	//	for _, vv := range keyValue {
-	//		arr := strings.Split(vv, "=")
-	//		if len(arr) < 2 {
-	//			continue
-	//		}
-	//		nums, _ = strconv.Atoi(arr[1])
-	//		res[arr[0]] = nums
-	//	}
-	//}
+	keySpace, err := rdb.Info(context.Background(), "keyspace").Result()
+	if err != nil {
+		fmt.Printf("数据执行有误" + err.Error())
+	}
+	dbList := strings.Split(keySpace, "\r\n")
+	for i, v := range dbList {
+		if i == 0 || v == "" {
+			continue
+		}
+		key := strings.Split(v, ":")
+		if len(key) < 2 {
+			continue
+		}
+		keyValue := strings.Split(key[1], ",")
+		for _, vv := range keyValue {
+			arr := strings.Split(vv, "=")
+			if len(arr) < 2 {
+				continue
+			}
+			nums, _ = strconv.Atoi(arr[1])
+			if arr[0] == "keys" {
+				res[key[0]] = nums
+			}
+		}
+	}
 	// config get 获取数据库个数
 	result, _ := rdb.ConfigGet(context.Background(), "databases").Result()
 
-	fmt.Printf("result:%v\n", result)
+	n, _ := strconv.Atoi(result[1].(string))
+	dbs := make([]*define.DbItem, n)
+	for i, _ := range dbs {
+		item := &define.DbItem{
+			Key:    fmt.Sprintf("key%d", i),
+			Number: 0,
+		}
+
+		if n, ok := res[fmt.Sprintf("key%d", i)]; ok {
+			item.Number = n
+		}
+		dbs[i] = item
+	}
+	fmt.Printf("dbs:%#v\n", dbs)
 }
