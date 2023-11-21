423dbf6b8e334b1e89e1e56b7ae9561aa30d5a7d
veneno <1929750203@qq.com>
2023-11-21 15:25:02 +0800

    document：icon

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -1,5 +1,41 @@
 export namespace define {
 	
+	export class AddHash {
+	    conn_identity: string;
+	    key: string;
+	    value: {[key: string]: string};
+	    ttl: number;
+	
+	    static createFrom(source: any = {}) {
+	        return new AddHash(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.conn_identity = source["conn_identity"];
+	        this.key = source["key"];
+	        this.value = source["value"];
+	        this.ttl = source["ttl"];
+	    }
+	}
+	export class AddUpdateString {
+	    conn_identity: string;
+	    key: string;
+	    value: string;
+	    ttl: number;
+	
+	    static createFrom(source: any = {}) {
+	        return new AddUpdateString(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.conn_identity = source["conn_identity"];
+	        this.key = source["key"];
+	        this.value = source["value"];
+	        this.ttl = source["ttl"];
+	    }
+	}
 	export class Connection {
 	    identity: string;
 	    name: string;
@@ -24,6 +60,50 @@ export namespace define {
 	        this.type = source["type"];
 	    }
 	}
+	export class DelHash {
+	    conn_identity: string;
+	    key: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new DelHash(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.conn_identity = source["conn_identity"];
+	        this.key = source["key"];
+	    }
+	}
+	export class DelHashItem {
+	    conn_identity: string;
+	    key: string;
+	    field: string[];
+	
+	    static createFrom(source: any = {}) {
+	        return new DelHashItem(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.conn_identity = source["conn_identity"];
+	        this.key = source["key"];
+	        this.field = source["field"];
+	    }
+	}
+	export class DelString {
+	    conn_identity: string;
+	    key: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new DelString(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.conn_identity = source["conn_identity"];
+	        this.key = source["key"];
+	    }
+	}
 	export class M {
 	    code: number;
 	    msg: string;
@@ -38,6 +118,40 @@ export namespace define {
 	        this.code = source["code"];
 	        this.msg = source["msg"];
 	        this.data = source["data"];
+	    }
+	}
+	export class SearchKey {
+	    conn_identity: string;
+	    db: number;
+	    keyword: string;
+	    keyType: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new SearchKey(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.conn_identity = source["conn_identity"];
+	        this.db = source["db"];
+	        this.keyword = source["keyword"];
+	        this.keyType = source["keyType"];
+	    }
+	}
+	export class UpdateHashItem {
+	    conn_identity: string;
+	    key: string;
+	    field: {[key: string]: string};
+	
+	    static createFrom(source: any = {}) {
+	        return new UpdateHashItem(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.conn_identity = source["conn_identity"];
+	        this.key = source["key"];
+	        this.field = source["field"];
 	    }
 	}
 
